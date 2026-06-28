742c8d8b62007f27c155cef5a744b1319709e68b
hjcanad <hjcanad@gmail.com>
2026-06-28 06:09:32 -0500

    added code

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-// template_5l0tmbr
-// service_if43qnv
-// QBleevghv3AtMChmb
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
 
- function contact(event) {
+function moveBackground(event) {
+   const shapes = document.querySelectorAll(".shape");
+   const x = event.clientX * scaleFactor; 
+   const y = event.clientY * scaleFactor;
+  
+   for (let i = 0;i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+   }
+}
+
+function toggleContrast () {
+    contrastToggle = !contrastToggle;
+    if  (contrastToggle) {
+ document.body.classList.add("dark-theme") 
+}
+else {
+ document.body.classList.remove("dark-theme")   
+}
+}
+
+function contact(event) {
     event.preventDefault();
 
 const loading = document.querySelector('.modal__overlay--loading');
@@ -29,7 +51,6 @@ loading.classList.add("modal__overlay--visible");
   })
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false
